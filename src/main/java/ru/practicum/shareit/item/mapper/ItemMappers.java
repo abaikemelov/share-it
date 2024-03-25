861840bfae82e56e2cdf4abede80edcc5accbbf1
abaikemelov <120552687+abaikemelov@users.noreply.github.com>
2024-03-25 12:42:58 +0600

    add mapper

---
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMappers.java
@@ -0,0 +1,31 @@
+package ru.practicum.shareit.item.mapper;
+
+import lombok.experimental.UtilityClass;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+@UtilityClass
+public class ItemMappers {
+    public static ItemDto toItemDto(Item item) {
+        return ItemDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.getAvailable())
+                .ownerId(item.getOwner().getId())
+                .requestId(item.getRequestId())
+                .build();
+    }
+
+    public static Item toItem(ItemDto itemDto) {
+        return Item.builder()
+                .id(itemDto.getId())
+                .name(itemDto.getName())
+                .description(itemDto.getDescription())
+                .available(itemDto.getAvailable())
+                .owner(User.builder().id(itemDto.getOwnerId()).build())
+                .requestId(itemDto.getRequestId())
+                .build();
+    }
+}
